70c6ea3da7474abce85c3ecb74a48358442d62ea
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-10 21:39:07 -0300

    Jinja HTML templates

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-  return "<p>login</p>"
+  return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-  return "<p>logout</p>"
+  return '<p>Logout</p>'
+
 
 @auth.route('/sign-up')
 def sign_up():
-  return "<p>signup</p>"
+  return render_template('sign-up.html')
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 # this function will run whenever we go to the / route
 def home():
-  return "<h1>Test</h1>"
+  return render_template('home.html')
